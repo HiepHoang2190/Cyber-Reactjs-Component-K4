1f7390ae31ac51c3326f44c118fa30b74e6b57d4
Hiep Hoang <hieph@lotusoutsourcing.com>
2021-08-28 11:21:26 +0700

    ReactJs-K4:Lesson 1: hướng dẫn cấu trúc thư mục dự án và cài đặt thư viện

---
+++ react-component-k4/src/redux/configStore.js
@@ -0,0 +1,13 @@
+import {combineReducers, createStore} from 'redux';
+
+
+
+const rootReducer = combineReducers({
+    //reducer khai báo tại đây
+})
+
+
+const store = createStore(rootReducer);
+
+
+export default store;
