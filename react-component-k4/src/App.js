fca42e473947295e6f4da9ec1b063e869ad7651e
Hiep Hoang <hieph@lotusoutsourcing.com>
2021-08-28 15:20:46 +0700

    ReactJs-K4: lesson 2: Hướng dẫn chia route cho page component

--- react-component-k4/src/App.js
+++ react-component-k4/src/App.js
@@ -1,11 +1,22 @@
 
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Header from './components/Home/Header/Header';
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      hello react 
-    </div>
+    <BrowserRouter>
+    <Header/>
+      <Switch>
+        <Route exact path='/home' component={Home} />
+        <Route exact path='/contact' component={Contact} />
+        <Route exact path='/about' component={About} />
+        <Route exact path='/' component={Home} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
