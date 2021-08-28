0d984cb86e65222d006dd61813a12e7eb20777ff
Hiep Hoang <hieph@lotusoutsourcing.com>
2021-08-28 15:49:09 +0700

    ReactJS-K4: lesson 3: Hướng dẫn sử dụng thẻ NavLink

--- react-component-k4/src/components/Home/Header/Header.js
+++ react-component-k4/src/components/Home/Header/Header.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import './Header.css';
 
 export default function Header() {
     return (
@@ -10,17 +12,14 @@ export default function Header() {
             <div className="collapse navbar-collapse" id="collapsibleNavId">
                 <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
                     <li className="nav-item active">
-                        <a className="nav-link" href="/home">Home <span className="sr-only">(current)</span></a>
+                        <NavLink activeClassName="activeNavItem " activeStyle={{fontWeight:'bold'}} className="nav-link" to="/home">Home <span className="sr-only">(current)</span></NavLink>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/about">About</a>
+                        <NavLink activeClassName="activeNavItem" activeStyle={{fontWeight:'bold'}} className="nav-link" to="/about">About</NavLink>
                     </li>
                     <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Contact</a>
-                        <div className="dropdown-menu" aria-labelledby="dropdownId">
-                            <a className="dropdown-item" href="#">Action 1</a>
-                            <a className="dropdown-item" href="#">Action 2</a>
-                        </div>
+                    <NavLink activeClassName="activeNavItem" activeStyle={{fontWeight:'bold'}} className="nav-link" to="/contact">Contact</NavLink>
+
                     </li>
                 </ul>
                 <form className="form-inline my-2 my-lg-0">
