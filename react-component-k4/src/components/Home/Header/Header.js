2a811a0d6313231ecf206605ce8cf78f9aac120b
Hiep Hoang <hieph@lotusoutsourcing.com>
2021-08-28 17:37:41 +0700

    ReactJs-K4: Lesson 4: HƯớng dẫn sử dụng thẻ Redirect

--- react-component-k4/src/components/Home/Header/Header.js
+++ react-component-k4/src/components/Home/Header/Header.js
@@ -25,6 +25,10 @@ export default function Header() {
                     <NavLink activeClassName="activeNavItem" activeStyle={{fontWeight:'bold'}} className="nav-link" to="/login">Login</NavLink>
 
                     </li>
+                    <li className="nav-item dropdown">
+                    <NavLink activeClassName="activeNavItem" activeStyle={{fontWeight:'bold'}} className="nav-link" to="/profile">Profile</NavLink>
+
+                    </li>
                 </ul>
                 <form className="form-inline my-2 my-lg-0">
                     <input className="form-control mr-sm-2" type="text" placeholder="Search" />
