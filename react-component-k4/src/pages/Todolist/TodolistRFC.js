012e6be16bfebab220e62b2954b582cbadd3c4e3
Hiep Hoang <hieph@lotusoutsourcing.com>
2021-09-04 10:51:38 +0700

    ReactJs-K4: Lesson 12:Axios với react function component (react hook- tính năng get task)

--- react-component-k4/src/pages/Todolist/TodolistRFC.js
+++ react-component-k4/src/pages/Todolist/TodolistRFC.js
@@ -1,61 +1,158 @@
-import React from 'react'
-import style from './Todolist.css';
 import Axios from 'axios';
-export default function TodolistRFC() {
-    return (
-        <div>
-            <div className="card">
-                <div className="card__header">
-                    <img src="./img/X2oObC4.png" />
+import React, { useEffect, useState } from 'react'
+
+export default function TodolistRFC(props) {
+
+    let [state,setState] = useState({
+        taskList: [],
+        values: {
+            taskName: ''
+        },
+        errors: {
+            taskName: ''
+        }
+    });
+
+    const handleChange = (e)=> {
+        let { value, name } = e.target;
+        console.log(value, name);
+        let newValues = { ...state.values };
+
+        newValues = { ...newValues, [name]: value };
+
+        let newErrors = { ...state.errors };
+
+        let regexString = /^[a-z A-Z]+$/;
+
+        if (!regexString.test(value) || value.trim() === '') {
+            newErrors[name] = name + ' invalid !';
+        } else {
+            newErrors[name] = '';
+        }
+
+
+        setState({
+            ...state,
+            values: newValues,
+            errors: newErrors
+        })
+    } 
+
+
+    const getTaskList = () => {
+        let promise = Axios({
+            url: 'http://svcy.myclass.vn/api/ToDoList/GetAllTask',
+            method: 'GET'
+        });
+
+        promise.then((result) => {
+            console.log(result.data);
+            //Nếu gọi api lấy về kết quả thành công 
+            //=> set lại state của component
+            setState({
+                ...state,
+                taskList: result.data
+            })
+
+            console.log('thành công')
+        });
+
+
+        promise.catch((err) => {
+            console.log('thất bại')
+
+            console.log(err.response.data)
+        });
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();//Chặn sự kiện reload lại trang
+    }
+
+    useEffect(() => {
+            getTaskList();
+
+
+        return () => {
+            
+        }
+    }, [])
+
+
+    const renderTaskToDo = () => {
+        return state.taskList.filter(item => !item.status).map((item, index) => {
+            return <li key={index}>
+                <span>{item.taskName}</span>
+                <div className="buttons">
+                    <button className="remove" type="button" onClick={() => {
+                        // delTask(item.taskName)
+                    }}>
+                        <i className="fa fa-trash-alt" />
+                    </button>
+                    <button type="button" className="complete" onClick={()=>{
+                        // checkTask(item.taskName)
+                    }}>
+                        <i className="far fa-check-circle" />
+                        <i className="fas fa-check-circle" />
+                    </button>
                 </div>
-                {/* <h2>hello!</h2> */}
-                <div className="card__body">
-                    <div className="card__content">
-                        <div className="card__title">
-                            <h2>My Tasks</h2>
-                            <p>September 9,2020</p>
-                        </div>
-                        <div className="card__add">
-                            <input id="newTask" type="text" placeholder="Enter an activity..." />
-                            <button id="addItem">
-                                <i className="fa fa-plus" />
-                            </button>
-                        </div>
-                        <div className="card__todo">
-                            {/* Uncompleted tasks */}
-                            <ul className="todo" id="todo">
-                                <li>
-                                    <span>Đi ngủ</span>
-                                    <div className="buttons">
-                                        <button className="remove">
-                                            <i className="fa fa-trash-alt" />
-                                        </button>
-                                        <button className="complete">
-                                            <i className="far fa-check-circle" />
-                                            <i className="fas fa-check-circle" />
-                                        </button>
-                                    </div>
-                                </li>
-                            </ul>
-                            {/* Completed tasks */}
-                            <ul className="todo" id="completed">
-                                <li>
-                                    <span>Ăn sáng</span>
-                                    <div className="buttons">
-                                        <button className="remove">
-                                            <i className="fa fa-trash-alt" />
-                                        </button>
-                                        <button className="complete">
-                                            <i className="far fa-check-circle" />
-                                            <i className="fas fa-check-circle" />
-                                        </button>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
+            </li>
+        })
+    }
+
+    
+    const renderTaskToDoDone = () => {
+        return state.taskList.filter(item => item.status).map((item, index) => {
+            return <li key={index}>
+                <span>{item.taskName}</span>
+                <div className="buttons">
+                    <button className="remove" type="button" onClick={() => {
+                        // delTask(item.taskName)
+                    }}>
+                        <i className="fa fa-trash-alt" />
+                    </button>
+                    <button  type="button" className="complete" onClick={()=>{
+                        // rejectTask(item.taskName)
+                    }}>
+                        <i className="far fa-undo" />
+                        <i className="fas fa-undo" />
+                    </button>
+                </div>
+            </li>
+        })
+    }
+
+
+    return (
+        <div className="card">
+            <div className="card__header">
+            <img src="./img/X2oObC4.png" />
+            </div>
+            {/* <h2>hello!</h2> */}
+            <form className="card__body" onSubmit={handleSubmit}>
+                <div className="card__content">
+                    <div className="card__title">
+                        <h2>My Tasks</h2>
+                        <p>September 9,2020</p>
+                    </div>
+                    <div className="card__add">
+                        <input id="newTask" name="taskName" type="text" placeholder="Enter an activity..." onChange={handleChange} />
+                        <button id="addItem">
+                            <i className="fa fa-plus" />
+                        </button>
+                    </div>
+                    <div className="card__todo">
+                        {/* Uncompleted tasks */}
+                        <ul className="todo" id="todo">
+                           {renderTaskToDo()}
+                        </ul>
+                        {/* Completed tasks */}
+                        <ul className="todo" id="completed">
+                            {renderTaskToDoDone()}
+                        </ul>
                     </div>
                 </div>
-            </div>
+            </form>
         </div>
 
     )
